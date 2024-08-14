3d2758ca624ed58b9e888aa5d84bc4f1c119bcec
Arka Basak <basakarka3@gmail.com>
2024-08-14 16:54:03 -0700

    Ex3.2 Submission

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('hello worlds')
 const express = require('express')
 const app = express()
 
@@ -32,8 +31,14 @@ app.get('/', (request, response) =>{
 app.get('/api/persons', (request, response)=>{
     response.json(persons)
 })
+app.get('/info', (request, response)=>{
+  const now = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${now.toString()}`)
+
+})
 
 const PORT = 3001
 app.listen(PORT, ()=>{
+  currentTime = 
     console.log(`Server running on port ${PORT}`)
 })
