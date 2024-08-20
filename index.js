dab1b96089f0055eeb48238ad07b433fb9fc74ad
Arka Basak <basakarka3@gmail.com>
2024-08-19 19:25:53 -0700

    Ex3.15 Submission

--- index.js
+++ index.js
@@ -58,16 +58,16 @@ app.get('/api/persons', (request, response)=>{
 })
 
 
-app.get('/api/persons/:id', (request, response)=>{
+app.get('/api/persons/:id', (request, response ,next)=>{
   Person.findById(request.params.id)
     .then(persons =>{
-      response.json(persons)
+      if (persons){
+        response.json(persons)
+      }else{
+        response.status(404).end()
+      }
     })
-    .catch(error=>{
-      response.status(404).json({
-        error: `Could not find person: ${request.params.id}`
-    }).end()
-  })
+    .catch(error=>next(error))
 })
 
 app.post('/api/persons',(request, response)=>{
@@ -92,11 +92,27 @@ app.post('/api/persons',(request, response)=>{
   })
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-  const id = request.params.id
-  persons = persons.filter(person =>person.id !== id )
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response ,next)=>{
+  Person.findByIdAndDelete(request.params.id)
+    .then(result =>{
+      response.status(204).end()
+    })
+    .catch(error=>next(error))
+  // const id = request.params.id
+  // persons = persons.filter(person =>person.id !== id )
+  // response.status(204).end()
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
